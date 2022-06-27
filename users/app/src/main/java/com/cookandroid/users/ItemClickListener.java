5eb2f6bcba49eba040017e9062813b8dc6c98557
김종문 <51452996+kin8089@users.noreply.github.com>
2022-06-27 10:16:43 +0900

    Delete users directory

--- users/app/src/main/java/com/cookandroid/users/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.cookandroid.users;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClickListener(View v, int position);
-}
--- users/app/src/main/java/com/cookandroid/users/RecyclerViewInterface.java
+++
@@ -1,5 +0,0 @@
-package com.cookandroid.users;
-
-public interface RecyclerViewInterface {
-    void onItemClick(int position);
-}
